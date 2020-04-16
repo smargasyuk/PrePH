75c87c816f8c89c53b492d84cbdd55456b421a66
kalmSveta <svetlana.kalmykova@skoltech.ru>
2020-04-16 21:23:52 +0300

    added

---
+++ src/MakeBedForVirusGenome.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python2
+import re, sys, getopt, itertools, binascii, time, subprocess
+from functools import partial
+sys.path.append('../tools/')
+from Bio import SeqIO
+import pandas as pd
+from Bio.Seq import Seq
+
+def split_overlap(seq, size, overlap):        
+    for i in range(0, len(seq) - overlap, size - overlap):  
+        yield(seq[i:i + size])
+def split_overlap_i(seq, size, overlap):        
+    for i in range(0, len(seq) - overlap, size - overlap):  
+        yield(i)
+def find_end(row):
+    return(row['chromStart'] + len(row['sequences']) - 1)
+    
+def main(argv):
+    path_to_input = ''
+    path_to_output = ''
+    size = 1000
+    overlap = 30
+
+    try:
+        opts, args = getopt.getopt(argv, "i:o:s:v:",
+                                   ["help=", "input=", "output=", "size=", "overlap="])
+    except getopt.GetoptError:
+        print('FindPanhandles.py -i <input.fa> -o <output.bed> -s <size> -v <overlap>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print('FindPanhandles.py -i <input.fa> -o <output.bed> -s <size> -v <overlap>')
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            path_to_input = arg
+        elif opt in ("-o", "--output"):
+            path_to_output = arg
+        elif opt in ("-s", "--size"):
+            size = int(arg)
+        elif opt in ("-v", "--overlap"):
+            overlap = int(arg)
+
+    records = SeqIO.read(path_to_input, "fasta")
+    seq = str(records.seq)
+    seqs = [x for x in split_overlap(seq, size, overlap)]
+    df = pd.DataFrame(seqs, columns=['sequences'])
+    df['chrom'] = 'chr1'
+    df['chromStart'] = [x+1 for x in split_overlap_i(seq, size, overlap)] 
+    df['chromEnd'] = df.apply(find_end, axis = 1)
+    df['name'] = range(1,df.shape[0]+1)
+    df['score'] = 1
+    df['strand'] = '+'
+    df = df[['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand', 'sequences']]
+    df.to_csv(path_to_output, sep='\t', index=False, header=True)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
